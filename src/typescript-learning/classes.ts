bc73e517be9a8a1ac574324d0577325b9a3b0571
Siddharth Kulkarni <sidd.kulk@gmail.com>
2025-05-03 15:56:59 +0530

    Adding folder and sample class for typescript practice

---
+++ src/typescript-learning/classes.ts
@@ -0,0 +1,17 @@
+
+
+class Queue<T> {
+
+    add(element: T): void{
+        // Adding element to the Queue
+        
+    }
+
+    peek(): T{
+        return null
+    }
+
+    pop(): T{
+        return null
+    }
+}
